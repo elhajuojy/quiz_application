cc44f5f81ee0f0dbeb80de233763b806a030f513
elhajuojy <souirimehdi311@gmail.com>
2022-05-24 00:48:49 +0100

    Create some folders

---
+++ lib/question.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/src/foundation/key.dart';
+import 'package:flutter/src/widgets/framework.dart';
+
+class Questionn extends StatelessWidget {
+  //sconst Questionn({Key? key}) : super(key: key);
+  final String questionText;
+  Questionn(this.questionText);
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: double.infinity,
+      margin: const EdgeInsets.all(10),
+      child: Text(
+        questionText,
+        style: TextStyle(fontSize: 20),
+        textAlign: TextAlign.center,
+      ),
+    );
+  }
+}
